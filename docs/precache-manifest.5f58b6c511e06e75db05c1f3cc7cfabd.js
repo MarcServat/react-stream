3d86edbc93cbb43ef45454d1785bda93c1ed3b10
Marc Servat <marc.servat@smartcommerce.co>
2019-04-01 09:21:52 +0200

    deploy

---
+++ docs/precache-manifest.5f58b6c511e06e75db05c1f3cc7cfabd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f81ab55cc3a1fe71351b",
+    "url": "/static/js/main.f81ab55c.chunk.js"
+  },
+  {
+    "revision": "007a5f8520e201c4aeb0",
+    "url": "/static/js/2.007a5f85.chunk.js"
+  },
+  {
+    "revision": "02aa04dd37aa65c097833e7ddb1b3858",
+    "url": "/index.html"
+  }
+];
