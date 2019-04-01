ec0a085ebb7c1ffb0aab2346c0af5b9b8c5c6471
Marc Servat <marc.servat@smartcommerce.co>
2019-04-01 09:31:53 +0200

    Updates

---
+++ precache-manifest.c0e623862e910e7901b1c06b121a8d34.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3289932bfc6541d0f7dc",
+    "url": "/react-stream/static/js/runtime~main.3289932b.js"
+  },
+  {
+    "revision": "f81ab55cc3a1fe71351b",
+    "url": "/react-stream/static/js/main.f81ab55c.chunk.js"
+  },
+  {
+    "revision": "007a5f8520e201c4aeb0",
+    "url": "/react-stream/static/js/2.007a5f85.chunk.js"
+  },
+  {
+    "revision": "768fd235a4bf55ddf1719ac157b4b977",
+    "url": "/react-stream/index.html"
+  }
+];
